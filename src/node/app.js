bcb95af11897e262a89e1a4ee5c9354aa1e78ea9
白雾三语 <32354856+baiwusanyu-c@users.noreply.github.com>
2023-06-26 21:23:44 +0800

    src/node/app.js comment

--- src/node/app.js
+++ src/node/app.js
@@ -1,6 +1,9 @@
 import { createApp, h } from 'vue'
+
+// 虚拟模块。这是客户端的根组件
 import App from 'virtual:vue-devtools-path:Container.vue'
 
+// TODO: 猜测这是在客户端的事件执行方法，用于执行服务端的事件消息等
 const hook = window.__VUE_DEVTOOLS_GLOBAL_HOOK__ ??= {
   events: new Map(),
   on(event, fn) {
@@ -15,6 +18,7 @@ const hook = window.__VUE_DEVTOOLS_GLOBAL_HOOK__ ??= {
   },
 }
 
+// 挂载客户端，这段代码会被发送到浏览器中并执行
 function load() {
   const CONTAINER_ID = '__vue-devtools-container__'
   const el = document.createElement('div')
