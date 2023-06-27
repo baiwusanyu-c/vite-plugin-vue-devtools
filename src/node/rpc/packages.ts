d73a1ea65ca3d1e51977c88023d23e22d010521d
baiwusanyu-c <740132583@qq.com>
2023-06-27 18:07:48 +0800

    src/node/rpc/packages.ts comment

--- src/node/rpc/packages.ts
+++ src/node/rpc/packages.ts
@@ -2,8 +2,10 @@ import { promises as fsp } from 'node:fs'
 import { resolve } from 'pathe'
 import type { PackageMeta } from '../../types'
 
+// 获取项目包依赖信息vue 版本等
 export async function getPackages(root: string) {
   // TODO: support monorepo workspace ?
+  // 很简单，就是通过 fs 读取 package.json 字段，然后对其dep做分类
   const pkgPath = resolve(root, 'package.json')
   const data = JSON.parse(await fsp.readFile(pkgPath, 'utf-8').catch(() => '{}'))
   const categorizedPackages = {}
